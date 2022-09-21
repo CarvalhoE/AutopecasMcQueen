807bcc7d6b66d63369c547c9aa609725296256b2
Lukas Reis de Oliveira <lukas.oliveira@csprime.com.br>
2022-09-21 02:35:41 -0300

    Adicionado pasta de testes ao projeto

--- Testes/BasicCRUD/index.js
+++ Testes/BasicCRUD/index.js
@@ -1,25 +1,27 @@
-const myPromise = new Promise((resolve, reject) => {
+(async () => {
+    const clienteController = require("./controllers/clienteController");
 
-    const button = document.querySelector('#submit')
-    button.addEventListener('click', function() {
-        login();
-    });
-    
-    
-    const login = async () => {
-        let controller = require("./controllers/clienteController");
-        let nome = document.getElementById('nome').value;
-        let cpf = document.getElementById('cpf').value;
-        await controller.login({nome: nome, cpf: cpf});
-    }
-    
-})
+    await clienteController.insertClient({nome: "Rodrigo", cpf: "30264851870"});
+    console.log(await clienteController.selectClients());
+})();
 
-myPromise.then((data) => {
-    console.log(data);
-})
-// (async () => {
-//     // await cliente.insertClient({nome: "Rodrigo", cpf: "30264851870"});
+// const myPromise = new Promise((resolve, reject) => {
+
+//     const button = document.querySelector('#submit')
+//     button.addEventListener('click', function() {
+//         login();
+//     });
     
-//     // console.log(await cliente.selectClients());
-// })();
+    
+//     const login = async () => {
+//         let controller = require("./controllers/clienteController");
+//         let nome = document.getElementById('nome').value;
+//         let cpf = document.getElementById('cpf').value;
+//         await controller.login({nome: nome, cpf: cpf});
+//     }
+    
+// })
+
+// myPromise.then((data) => {
+//     console.log(data);
+// })
