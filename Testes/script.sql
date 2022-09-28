f63fea0efc02e5a77e12977e42ad02d4c5becbee
Lukas Reis de Oliveira <lukas.oliveira@csprime.com.br>
2022-09-28 02:01:58 -0300

    Script

--- Testes/script.sql
+++ Testes/script.sql
@@ -1,4 +1,5 @@
 Drop Table if exists Funcionario;
+Drop Table if exists Departamento;
 
 Create Table Funcionario
 (
@@ -9,9 +10,21 @@ Create Table Funcionario
    ,Constraint PK_ID_Funcionario	 	Primary Key (ID_Funcionario)
 );
 
+Create Table Departamento
+(
+	ID_Departamento 	Int 			auto_increment
+   ,DS_Departamento 	VarChar(40) 	Not Null
+   ,Constraint PK_ID_Departamento Primary Key (ID_Departamento)
+);
+
 Insert Into Funcionario (NM_Nome, DS_Login, NR_Senha) Values ('Lukas', 'Lukas', '1234');
 Insert Into Funcionario (NM_Nome, DS_Login, NR_Senha) Values ('Yasmin', 'Yasmin', '1234');
 Insert Into Funcionario (NM_Nome, DS_Login, NR_Senha) Values ('Karen', 'Karen', '1234');
+
+Insert Into Departamento (DS_Departamento) Values ('Financeiro');
+Insert Into Departamento (DS_Departamento) Values ('RH');
+Insert Into Departamento (DS_Departamento) Values ('Operacional');
+Insert Into Departamento (DS_Departamento) Values ('Gestão');
 
 Select *
 	From Funcionario
