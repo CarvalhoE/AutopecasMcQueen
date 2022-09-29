2e5320ec151cdcb3c01bf4a8e98f1e2b0250419f
Lukas Reis de Oliveira <lukas.oliveira@csprime.com.br>
2022-09-28 21:38:07 -0300

    Atualização

--- Testes/Login/public/js/cadastraFuncionario.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-var app = require('../../app');
-
-document.getElementById('departamento').addEventListener('load', () => {
-    const departamentoController = require('../../controllers/departamentoController');
-    app.get('/departamentos', departamentoController.findAll);
-});
--- webSystem/system_v2/routes/financeiro.js
+++ webSystem/system_v2/routes/financeiro.js
@@ -1,7 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/financeiro', function (req, res, next) {
+router.get('/financeiro/recebimentos', function (req, res, next) {
+    if (req.session.loggedin) {
+      res.render('finRecebimentos', {
+        name: req.session.name
+      });
+    } else {
+      req.flash('sucess', 'É necessário estar logado para acessar esta página');
+      res.redirect('/login')
+    }
+  });
+
+  router.get('/financeiro', function (req, res, next) {
+    if (req.session.loggedin) {
+      res.render('financeiro/finPagamentos', {
+        name: req.session.name
+      });
+    } else {
+      req.flash('sucess', 'É necessário estar logado para acessar esta página');
+      res.redirect('/login')
+    }
+  });
+
+  router.get('/financeiro', function (req, res, next) {
+    if (req.session.loggedin) {
+      res.render('financeiro/finRecebimentos', {
+        name: req.session.name
+      });
+    } else {
+      req.flash('sucess', 'É necessário estar logado para acessar esta página');
+      res.redirect('/login')
+    }
+  });
+
+  router.get('/financeiro', function (req, res, next) {
     if (req.session.loggedin) {
       res.render('financeiro/finRecebimentos', {
         name: req.session.name
