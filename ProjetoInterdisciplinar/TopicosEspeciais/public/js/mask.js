a7de8a1e49fd5ca7ed6990e959cb6271818d7f68
Lukas Reis de Oliveira <lukas.oliveira@csprime.com.br>
2022-11-07 00:32:30 -0300

    Atualização do projeto interdisciplinar

--- ProjetoInterdisciplinar/TopicosEspeciais/public/js/mask.js
+++ ProjetoInterdisciplinar/TopicosEspeciais/public/js/mask.js
@@ -1,21 +1,39 @@
-$(document).ready(function(){
-    $(  '.date'   ).inputmask(  '99/99/9999',           {removeMaskOnSubmit: true}  );
-    $(  '.time'   ).inputmask(  '99:99:99',             {removeMaskOnSubmit: true}  );
-    $(  '.cep'    ).inputmask(  '99999-999',            {removeMaskOnSubmit: true}  );
-    $(  '.phone'  ).inputmask(  '(99) 99999-9999',      {removeMaskOnSubmit: true}  );
-    $(  '.cpf'    ).inputmask(  '999.999.999-99',       {removeMaskOnSubmit: true}  );
-    $(  '.cnpj'   ).inputmask(  '99.999.999/9999-99',   {removeMaskOnSubmit: true}  );
-    $(  '.sku'    ).inputmask(  'AA-99999999',          {removeMaskOnSubmit: true}  );
-    $(  '.money'  ).inputmask(  'decimal', {
-                                               'alias': 'numeric',
-                                               'groupSeparator': ',',
-                                               'autoGroup': true,
-                                               'digits': 2,
-                                               'radixPoint': ".",
-                                               'digitsOptional': false,
-                                               'allowMinus': false,
-                                               'prefix': 'R$ ',
-                                               'placeholder': '',
-                                               removeMaskOnSubmit: true
-                                           });
+$(document).ready(function () {
+    $('.date').inputmask('99/99/9999', {
+        removeMaskOnSubmit: true
+    });
+    $('.time').inputmask('99:99:99', {
+        removeMaskOnSubmit: true
+    });
+    $('.cep').inputmask('99999-999', {
+        removeMaskOnSubmit: true
+    });
+    $('.phone').inputmask('(99) 99999-9999', {
+        removeMaskOnSubmit: true
+    });
+    $('.landline').inputmask('(99) 9999-9999', {
+        removeMaskOnSubmit: true
+    });
+    $('.cpf').inputmask('999.999.999-99', {
+        removeMaskOnSubmit: true
+    });
+    $('.cnpj').inputmask('99.999.999/9999-99', {
+        removeMaskOnSubmit: true
+    });
+    $('.sku').inputmask('AA-99999999', {
+        removeMaskOnSubmit: true
+    });
+    $('.money').inputmask('decimal', {
+        'alias': 'numeric',
+        'groupSeparator': '.',
+        'autoGroup': true,
+        'digits': 2,
+        'radixPoint': ",",
+        'digitsOptional': false,
+        'allowMinus': false,
+        'prefix': 'R$ ',
+        'placeholder': '',
+        'rightAlign': false,
+        removeMaskOnSubmit: true
+    });
 });
---
+++ ProjetoInterdisciplinar/TopicosEspeciais/public/js/vendas.js
@@ -0,0 +1,86 @@
+//#region Inicialização de variáveis
+const produtos = new Array();
+//#endregion
+
+//#region Retornar tabela ao HTML
+let displayHtmlData = () => {
+    let html = "";
+    let valorTotal = 0;
+    for (let i = 0; i < produtos.length; i++) {
+        html += `<tr id="row${i}">
+                     <td>${produtos[i].sku}</td>
+                     <td>${produtos[i].nome}</td>
+                     <td>${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produtos[i].valor)}</td>
+                     <td>${produtos[i].quantidade}</td>
+                     <td>${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(produtos[i].valorTotal)}</td>
+                     <td><button id="${i}" class="cancel" type="button" onclick="remove_OnClick(${produtos[i].id})"><i class="fa fa-times" /></button></td>
+                 </tr>`;
+        valorTotal += produtos[i].valorTotal;
+    }
+    $('#tableBody').html(html);
+    $('#lValorTotal').html(`Valor Total: ${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorTotal)}`);
+    $('#valorTotal').val(valorTotal).change();
+}
+//#endregion
+
+//#region Adicionar Produto
+let add_OnClick = () => {
+    //#region Recuperação de Dados
+    let produto_Selected = $('#produtoNovaVenda').val();
+    let produtoDetails = produto_Selected.split('|');
+    let id = produtoDetails[3];
+    let sku = produtoDetails[0];
+    let nome = produtoDetails[1];
+    let valor = Number(produtoDetails[2]);
+    let quantidade = Number($('#quantidade').val());
+    //#endregion
+    
+    if (produtos.length == 0) {
+        produtos.push({
+            id: id,
+            sku: sku,
+            nome: nome,
+            valor: valor,
+            quantidade: quantidade,
+            valorTotal: quantidade * valor
+        });
+    } else {
+        let index = produtos.findIndex(obj => obj.sku == sku)
+        if (index >= 0) {
+            produtos[index].quantidade += quantidade;
+            produtos[index].valorTotal = produtos[index].quantidade * valor;
+        } else {
+            produtos.push({
+                id: id,
+                sku: sku,
+                nome: nome,
+                valor: valor,
+                quantidade: quantidade,
+                valorTotal: quantidade * valor
+            });
+        }
+    }
+    $('#produtos').val(JSON.stringify(produtos, null)).change();
+    displayHtmlData()
+};
+//#endregion
+
+//#region Validação de forma de pagamento.
+let formaPagamento = $('#formaPagamento');
+formaPagamento.on('change', () => {
+    let numeroParcelas = $('#numeroParcelas');
+    formaPagamento.val() == "1" ? numeroParcelas.css({'visibility':'visible'}) : numeroParcelas.css({'visibility':'hidden'})
+});
+//#endregion
+
+//#region Remoção de Produto da tabela e array.
+let remove_OnClick = (id) => {
+    
+    produtos.filter((a,i) => {
+        if (id == a.id) {
+            produtos.splice(i, 1);
+            displayHtmlData();
+        }
+    });
+}
+//#endregion
